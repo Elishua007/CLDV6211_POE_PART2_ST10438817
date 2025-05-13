7ef70c854997cf4c396810ace898dd2c0c742af1
Elishua Emmanuel Naidoo <166371667+Elishua007@users.noreply.github.com>
2025-05-13 16:26:03 +0200

    Add files via upload

---
+++ ViewModels/BookingHistory.cs
@@ -0,0 +1,16 @@
+﻿using EventEase_Booking_System.Models;
+
+namespace EventEase_Booking_System.ViewModels
+{
+    public class BookingHistory
+    {
+
+        public List<Booking>? Bookings { get; set; }
+
+        public string? FilterVenueName { get; set; }
+        public string? FilterEventName { get; set; }
+        public DateTime? FilterBookingStartDate { get; set; }
+        public DateTime? FilterBookingEndDate { get; set; }
+
+    }
+}
